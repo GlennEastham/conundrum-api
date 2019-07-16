519eb53ced6a6e77ae4718eb6c01f9ad1621d515
Glen Eastham <Glen.Eastham@gmail.com>
2019-07-16 23:37:08 +0100

    added swagger, YAML not setup

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 var express = require('express');
-
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'),
 app = express();
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+  info:{
+    title: 'Conundrum API',
+    version: '1.0.0',
+    description: 'Conundrum endpoints'
+  }, 
+  host: 'localhost:3000',
+  basePath: '/'
+}
+
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/*.js']
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
