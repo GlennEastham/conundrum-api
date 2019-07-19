7a0d543a30abe344752cc86e46a90fddca572c6d
Glen Eastham <Glen.Eastham@gmail.com>
2019-07-19 17:57:18 +0100

    Updated production URL for swagger

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerJSDoc = require('swagger-jsdoc')
 const bodyParser = require('body-parser')
 const app = express()
+const env = process.env.NODE_ENV || 'development'
 const port = process.env.PORT || 3000
 
 app.listen(port)
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+let host
+
+if (env === 'development') {
+  host = 'localhost:3000'
+} else {
+  host = 'conundrum-api.herokuapp.com'
+}
 
 const swaggerDefinition = {
   info: {
@@ -17,9 +26,10 @@ const swaggerDefinition = {
     version: '1.0.0',
     description: 'Conundrum endpoints'
   },
-  host: process.env.URL || 'localhost:3000',
+  host: host,
   basePath: '/'
 }
+
 const swaggerUIOptions = {
   customCss: '.swagger-ui .topbar { display: none }'
 }
